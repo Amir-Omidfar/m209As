79ea1bab8592553a03edc854657b4ef61f69eb66
Hannah <HannaHojaiji@users.noreply.github.com>
2019-12-03 18:46:00 -0800

    Add files via upload

---
+++ bakeOff2/processData2.py
@@ -0,0 +1,112 @@
+#Team:Amirali Omidfar, Hannaneh Hojaiji
+#Source for text to speech conversion https://pypi.org/project/pyttsx3/
+import csv
+import datetime as dt
+import numpy as np
+import random
+
+
+ax= []
+ay= []
+az= []
+Vix = []
+Viy = []
+xs = []
+vx = 0
+vy = 0
+
+minAx=0
+minAy=0
+minAz=0
+minVx=0
+minVy=0
+
+maxAx=0
+maxAy=0
+maxAz=0
+maxVx=0
+maxVy=0
+correct=["you did it right congrats!!","Excellent! That was the proper gesture",
+"Well done! You successfully triggered the device","Yup that's right!","Nicely triggered. Good job!"]
+
+slowDown=["Seems to me that you started early this time. Follow the LED carfeully",
+"You're too fast, next round you need to slow down", "oh oh, too fast!"]
+
+speedUp=["You need to perform the gesture faster","Almost no arm movement detected. You need to be more active",
+"Come on CamIoT is a mind reader.You need to move your arm",
+"Looks like you're falling behind, initiate your move right away when the LED is solid"]
+
+tryAgain=["Please try again", "Hmm, something went wrong, please try again."]
+
+def feedback(result):
+	if result ==" '[ True]'":
+		return 1.0 #random.choice(correct)
+	elif result ==" '[False]'":
+		return 0.0 #random.choice(incorrect)
+
+
+def processMyData():
+	#HH
+	with open('polishedData.txt') as csv_file:
+	    csv_reader = csv.reader(csv_file, delimiter=',')
+	    line_count = 0
+	    Rows=list(csv_reader)
+	    lastRow=Rows[-1]
+
+	
+	global ax,ay,az,Vix,Viy,xs,vx,vy
+	vx = 0
+	vy = 0
+	rawData=lastRow[42]
+	#result=feedback(str(rawData))
+	for j in range (14):
+		if (j<14):
+			ax.append(lastRow[3*j])
+			ax[j]=float(ax[j])
+
+			ay.append(lastRow[3*j+1])
+			ay[j]=float(ay[j])
+
+			az.append(lastRow[3*j+2])
+			az[j]=float(az[j])
+
+			Viy.append(ay[j]*(0.1) + vy)
+			vy = Viy[j]
+
+			Vix.append(ax[j]*(0.1) + vx)
+			vx = Vix[j]
+			#timeStamp=dt.datetime.now().strftime('%-S.%f')
+			#timeStampRounded=timeStamp[:-3]
+			#xs.append(timeStamp)
+			xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+	csv_file.close()
+	minAx=min(ax)
+	minAy=min(ay)
+	minAz=min(az)
+	minVx=min(Vix)
+	minVy=min(Viy)
+
+	maxAx=max(ax)
+	maxAy=max(ay)
+	maxAz=max(az)
+	maxVx=max(Vix)
+	maxVy=max(Viy)
+
+	if ((minAx<-15.0 and maxAx>-2.0) and (feedback(str(rawData))==1.0)):
+		result=random.choice(slowDown)
+
+	elif ((-15.0<minAx<-8.0 and -4.0<maxAx<0.0) and (feedback(str(rawData))==1.0)):
+		result=random.choice(correct)
+
+	elif ((minAx<-15.0 and maxAx>-2.0) and (feedback(str(rawData))==0.0)):
+		result=random.choice(slowDown)
+
+	elif ((-8.0<minAx<-15.0 and -4.0<maxAx<0.0) and (feedback(str(rawData))==0.0)):
+		result=random.choice(tryAgain)
+
+	else:
+		result=random.choice(speedUp)
+
+	return result
+
+
--- bakeOff2/pyGui.py
+++ bakeOff2/pyGui.py
@@ -1,96 +1,190 @@
+#Team:Amirali Omidfar, Hannaneh Hojaiji
+from sys import platform as sys_pf
+if sys_pf == 'darwin':
+    import matplotlib
+    matplotlib.use("TkAgg")
+
+import sys
+from tkinter import *  
 import tkinter as tk
+import tkinter.ttk
 import server2
-from server2 import recordData
+#from server2 import connectionInit
+import socket
+
+
+#Voice commands
+import pyttsx3
+
+
+import time 
+import numpy as np 
+from polishData import polishMyData
+command=0
+channel=2020
+
+import matplotlib 
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as pet
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+#import processData2
+from processData2 import processMyData
+from processData2 import ax,ay,az,Vix,Viy,xs,vx,vy
+############################################Server code
+
+
+def connectionInit():
+	receivedData=open("receivedData.txt","a")
+	receivedData.write("\n")
+	global channel
+	command=1
+	print("port: ",channel)
+	#channel = input('Channel:')
+	########################
+	time.sleep(2)
+	tServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	tServer.bind(('192.168.1.67', int(channel)))
+	tServer.listen(0)
+	connect,addr = tServer.accept()
+	###############while loop for displaying the data
+	
+	active=True
+	try:
+		
+		while active:
+			#command=input("Enter command")
+			if int(command) == 2 :
+				active=False
+				connect.send(b'0')
+			elif int(command) ==1 :
+				command=bytearray(str(command),'utf-8')
+				connect.send(command)
+				data=connect.recv(1024).decode()
+				command=2
+				if data:
+					#receivedData.write("\n")
+					#print("next line: ",data)
+					receivedData.write(data)
+					receivedData.write("\n")
+					print("received data: ", data)
+			else:
+				print("waiting")
+
+	finally:
+		print("closing connection")
+		#channel=channel+1
+		receivedData.close()
+		tServer.close()
+############################################End of Server code
+
+
+# Create the main window and Buttons 
+
+
+def gui_start():
+	global root
+	root = tk.Tk()
+	root.title("Explainable CamIoT")
+	root.geometry("800x800")
+
+	def conTestFunc():
+		return 5
+	
+	def close_window():
+		root.quit()
+
+	def restart():
+		python = sys.executable
+		os.excel(python,python,*sys.argv)
+
+	def connection_pressed():
+		connectionInit()
+		polishMyData()
+
+	def analysisFunc():
+		result=processMyData()
+		global ax,ay,az,Vix,Viy,xs,vx,vy
+		f = pet.figure(figsize=(5,5), dpi =100)
+		axe1 = f.add_subplot(1, 3, 1)
+		axe2 = f.add_subplot(1, 3, 2)
+		axe3 = f.add_subplot(1, 3, 3)
+		axe1.clear() 
+		axe2.clear()
+		axe3.clear() 
+		axe1.plot(ax, label = "Ax")   
+		axe2.plot(ay, label = "Ay")  
+		axe3.plot(az, label = "Az")   
+		axe1.plot(Viy, label = "Vy")
+		axe2.plot(Vix, label = "Vx")
+		axe1.set_ylabel('Processed Data') 
+		axe1.set_xlabel('time')
+		axe2.set_xlabel('time')
+		axe3.set_xlabel('time')
+		axe1.legend()
+		axe2.legend()
+		axe3.legend()
+		# Format plot
+		pet.xticks(rotation=45, ha='right')
+		pet.subplots_adjust(bottom=0.30)
+		canvas = FigureCanvasTkAgg(f, master=root)
+		canvas.draw()
+		canvas.get_tk_widget().grid(row=3,column=0,sticky=W)
+		#canvas._tkcanvas.grid(row=3,column=0,sticky=W)
+		resetButton.config(state="normal")
+		anLabel=tk.Label(root,text=result,borderwidth=2, relief="solid")
+		anLabel.grid(row=4,column=0,sticky=W)
+		ax.clear()
+		ay.clear()
+		az.clear()
+		Vix.clear()
+		Viy.clear()
+		xs.clear()
+		vx=0
+		vy=0
+		engine = pyttsx3.init()
+		engine.say(result)
+		engine.runAndWait()
+
+
+	
+	instLabel=tk.Label(root,text="Hit Record Data to begin. Then Follow the blinking LED to its permanent ON state and perform the gesture.")
+	resLabel=tk.Label(root,text="To see your result then press Show My Analysis")
+	conTestLabel=tk.Label(root,text="After you're done practicing the gesture. Click below for continious testing.")
+	
+	rdB=tk.Button(root, text="Record Data", command=connection_pressed)
+	analysisB=tk.Button(root,text="Show My Analysis",command=analysisFunc)
+	conTestB=tk.Button(root,text="continious Testing",command=conTestFunc)
+	resetButton=tk.Button(root,text="Try Again",command=refresh,state=DISABLED)
+	Exit = tk.Button(root, text="Exit the program", command = close_window)
 
 
 
-#HH Parameters
-import csv
-import datetime as dt
-import matplotlib.pyplot as plt
-fig = plt.figure()
-axe = fig.add_subplot(1, 1, 1)
-ax= []
-ay= []
-az= []
-Vix = []
-Viy = []
-#HH
+	# Lay out label
+	instLabel.grid(row=0,sticky=W)
+	#instLabel.place(relx=0.01,rely=0.01)
+	resLabel.grid(row=1,sticky=W)
+	#resLabel.place(relx=0.01,rely=0.04)
+	rdB.grid(row=2,column=0,sticky=W)
+	#rdB.place(relx=0.01, rely=0.07)
+	analysisB.grid(row=2,column=0)
+	resetButton.place(relx=0.01,rely=0.9)
+	Exit.place(relx=0.5,rely=0.9)
 
-# Create the main window
-root = tk.Tk()
-root.title("Explainable CamIoT")
-root.geometry("500x500")
+	tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=1, row=0, rowspan=5, sticky='ns')
 
-# Create label
-label = tk.Label(root, text="Data Recording: ")
-
-def close_window():
-	root.quit()
+	conTestLabel.grid(row=0,column=2,sticky=W+E)
+	conTestB.grid(row=1,column=2)
 
 
-B = tk.Button(root, text ="Press the button to record data", command = recordData)
-Exit = tk.Button(root, text="Exit the program", command = close_window)
+	# Run forever!
+	root.mainloop()
 
-#HH
-with open('receivedData.txt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    Rows=list(csv_reader)
-	Tot_rows=len(Rows)
-with open('receivedData.txt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for i in csv_reader:
-    	if line_count==Tot_rows:
-    		Viy = 0
-    		Vix = 0
-    		for j in range (15)
-			if (j<14):
-				ax[j]=row[3*j]
-				ay[j]=row[3*j+1]
-				az[j]=row[3*j+2]
-				Viy[j] = ay[j]*(0.1) + Viy
-			    Xiy[j] = Viy[j]*(0.1) + Xiy[j-1]
-		        Viy = Viy[j]
-		        Vix[j] = ax[j]*(0.1) + Vix
-			    Xiy[j] = Vix[j]*(0.1) + Xix[j-1]
-		        Vix = Vix[j]
+if __name__ == '__main__':
+    def refresh():
+        root.destroy()
+        gui_start()
+
+    gui_start()
 
 
-		        xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
-		        # Draw x and y lists
-			    axe.clear()
-			    # Format plot
-			    plt.xticks(rotation=45, ha='right')
-			    plt.subplots_adjust(bottom=0.30)
-				# plotting the line 1 points (Ax)  
-				axe.plot(xs, ax, label = "Ax")
-				# plotting the line 2 points (Ay)  
-				axe.plot(xs, ay, label = "Ay")
-				# plotting the line 3 points (Az)  
-				axe.plot(xs, az, label = "Az") 
-				# plotting the line 4 points (Vz)  
-				axe.plot(xs, Viy, label = "Vy") 
-				# naming the x axis 
-				plt.xlabel('time') 
-				# naming the y axis 
-				plt.ylabel('accelerometer') 
-				# giving a title to my graph 
-				plt.title('accelerometer bundle') 
-				# show a legend on the plot 
-				plt.legend()
-			else: 
-				print(f'Classified as {row[14]}.')
-
-		plt.show()
-		line_count+=1
-#HH
-
-# Lay out label
-label.pack()
-B.place(x=50, y=40)
-Exit.place(x=40, y=350)
-
-# Run forever!
-
-root.mainloop()
